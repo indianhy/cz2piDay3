eba39a9e3c84d5975ea1cfed4d0b837c8f4adc40
HYMANSHU <hymanshu97@gmail.com>
2020-07-17 18:13:34 +0530

    error removed

--- matrix.js
+++ matrix.js
@@ -2,7 +2,9 @@
 function multiply(a, b) {
   var aRows = a.length, aCols = a[0].length,
       bRows = b.length, bCols = b[0].length,
-      mul = new Array(aRows); 
+        mul = new Array(aRows);
+    if (aCols != bRows)
+        return "invalid input";
   for (var i = 0; i < aRows; i++) {
     mul[i] = new Array(bCols);
     for (var j = 0; j < bCols; j++) {
@@ -15,12 +17,6 @@ function multiply(a, b) {
   return mul;
 }
 
-function display(m) {
-  for (var i = 0; i < m.length; i++) {
-    console.log(m[i].join(' '));
-  }
-}
-
-var a = [[8, 3], [2, 4], [3, 6]],
+var a = [[8, 3, 7], [2, 4, 9], [3, 6, 0]],
     b = [[1, 2, 3], [4, 6, 8]];
-display(multiply(a,b))
+console.log(multiply(a,b))
